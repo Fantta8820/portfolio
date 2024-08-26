8d894978a7e3a0f0081ff6b668b83c04d161bcb2
Fantta8820 <andrevictor2512@gmail.com>
2024-08-25 23:31:25 -0300

    Made a responsive design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,13 @@
 
 <body data-theme="coffee" class="w-screen h-screen grid place-items-center">
     <main class="w-3/4 h-screen flex flex-col justify-center">
-        <h1 class="text-5xl font-bold uppercase text-center group">Olá, tudo bem? Meu nome é <span class="text-accent group-hover:text-secondary transition delay-50">André </span>"<span class="text-accent group-hover:text-secondary transition delay-50">Fantta</span>"<span class="text-accent group-hover:text-secondary transition delay-50">Garcia</span>!</h1>
-        <p class="pt-4 text-2xl text-center font-semibold">Infelizmente não existem projetos aqui ainda. Porém caso queira ver meu perfil no GitHub ou meu TCC fique à vontade!</p>
+        <h1 class="text-3xl lg:text-5xl font-bold uppercase text-center group">Olá, tudo bem? Meu nome é <span class="text-accent group-hover:text-secondary transition delay-50">André </span>"<span class="text-accent group-hover:text-secondary transition delay-50">Fantta</span>"<span class="text-accent group-hover:text-secondary transition delay-50">Garcia</span>!</h1>
+        <p class="pt-4 text-lg lg:text-2xl text-center font-semibold">Infelizmente não existem projetos aqui ainda. Porém caso queira ver meu perfil no GitHub ou meu TCC fique à vontade!</p>
         <div class="divider"></div>
-        <section class="max-w-full mx-auto *:btn *:btn-secondary *:w-32 *:mx-2 *:text-base">
-            <a href="https://www.swibly.com.br/" target="_blank" class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white"><Icon icon="mdi:koala" />Swibly</a>
-            <a href="https://github.com/Fantta8820" target="_blank" class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white"><Icon icon="mdi:github" />GitHub</a>
-            <a href="https://arkhon.swibly.com.br/" target="_blank" class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white"><Icon icon="mdi:pencil" />Arkhon</a>
+        <section class="w-full flex justify-center *:btn *:btn-secondary *:w-20 *:sm:w-28 *:lg:w-32 *:mx-2 *:text-base">
+            <button class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white"><a href="https://www.swibly.com.br/" class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white sm:flex sm:items-center sm:gap-2" target="_blank"><Icon class="w-full mx-auto" icon="mdi:koala" />Swibly</a></button>
+            <button class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white"><a href="https://github.com/Fantta8820" class="sm:flex sm:items-center sm:gap-2" target="_blank"><Icon class="w-full mx-auto" icon="mdi:github" />GitHub</a></button>
+            <button class="transition delay-50 hover:scale-110 hover:btn-primary hover:text-white"><a href="https://arkhon.swibly.com.br/" class="sm:flex sm:items-center sm:gap-2" target="_blank"><Icon class="w-full mx-auto" icon="mdi:pencil" />Arkhon</a></button>
         </section>
     </main>
 </body>
